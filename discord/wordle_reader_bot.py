bc39cc847282ddf606ce269163e17ab2435e7cc5
Sam Dupas <samgd1031@gmail.com>
2022-08-22 20:58:02 -0700

    added WordleResult class
    can determine if a message is a valid wordle result based on the header
    
    Will need to add ability to validate to stop cheaters

--- discord/wordle_reader_bot.py
+++ discord/wordle_reader_bot.py
@@ -1,6 +1,9 @@
 
 import discord
 import json
+import re
+
+from wordle_result import WordleResult
 
 
 # class describing the bot client
@@ -13,7 +16,24 @@ class WordleClient(discord.Client):
     # later will need to add code that parses wordle messages and does something interesting with them
     async def on_message(self, message):
         print(f"Message from {message.author} - {message.content}")
+        if self.is_valid_wordle(message):
+            print("\tValid Wordle Result!")
+        else:
+            print("\tNot valid Wordle Result!")
 
+    # check if a message string is a valid wordle result
+    def is_valid_wordle(self, message) -> bool:
+        content = message.content.split("\n")
+
+        isWordle = re.match(r"Wordle \d* [\dX]/6\*?",
+                            content[0])
+        
+        if isWordle:
+            wr = WordleResult(message.author, content[0])
+            print(wr)
+            return True
+        else:
+            return False
 
 
 # main part of code that runs when script is called
---
+++ discord/wordle_result.py
@@ -0,0 +1,13 @@
+
+# class describing a single wordle result
+class WordleResult():
+    
+    def __init__(self, player, header):
+        self.player = player
+        hdr = header.split()
+        self.puzzle_number = hdr[1]
+        self.hard_mode = True if hdr[2][-1] == '*' else False
+        self.num_guesses = -1 if hdr[2][0] == 'X' else int(hdr[2][0])
+    
+    def __repr__(self) -> str:
+        return f"Puzzle {self.puzzle_number} by {self.player} - guesses: {self.num_guesses} - hard mode: {self.hard_mode}"
