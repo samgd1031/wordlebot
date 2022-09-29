5f5969856be2f6db9e7230ee56694769c9dc5413
Sam Dupas <samgd1031@gmail.com>
2022-09-28 19:21:51 -0700

    changed message for duplicate wordles

--- discord_bot/src/wordle_client/wordle_client.py
+++ discord_bot/src/wordle_client/wordle_client.py
@@ -38,7 +38,7 @@ class WordleClient(discord.Client):
                 self.mongo_collection.insert_one(wr.to_dict())
                 response = wr.__repr__()
             except pymongo.errors.DuplicateKeyError:
-                response = f"Duplicate entry not added to database (id: {wr.to_dict()['_id']})"
+                response = f"Duplicate entry for {wr.puzzle_number} added to database.)"
 
             await message.channel.send(response)
         else:
