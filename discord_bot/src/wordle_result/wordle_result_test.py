9ba1487cdbadbf206f963c1b597bcd3e72e50983
Sam Dupas <samgd1031@gmail.com>
2022-10-04 23:06:33 -0700

    fixed unit test

--- discord_bot/src/wordle_result/wordle_result_test.py
+++ discord_bot/src/wordle_result/wordle_result_test.py
@@ -2,21 +2,15 @@ import json
 from wordle_result import WordleResult
 import datetime as dt
 
-# mocks a discord message class
-class MessageMock():
-    def __init__(self, author, created, header):
-        self.author = author
-        self.created_at = created
-        self.header = header
-
 # test if wordle results jsonify correctly
 def test_jsonification():
-    msg = MessageMock("xX_JohnDoe_Xx", dt.datetime(2022,1,1,0,0,0), "Wordle 123 1/6")
-    wr = WordleResult(msg, msg.header)
-    dc = {"player":"xX_JohnDoe_Xx",
-        "dt":"2022-01-01T00:00:00",
-        "puzzle_number":"wordle_123",
-        "hard_mode":False,
-        "num_guesses":1,
-        "solved":True}
-    assert(wr.toJSON() == json.dumps(dc))
+    msg = {"player":"xX_JohnDoe_Xx_1234", 
+            "time":dt.datetime(2022,1,1,0,0,0,tzinfo=dt.timezone.utc),
+            "puzzle":"wordle_123",
+            "hard_mode": False,
+            "num_guesses": 1,
+            "solved": True,
+            "_id":"xX_JohnDoe_Xx_1234_wordle_123"
+    }
+    wr = WordleResult(msg)
+    assert(wr.to_dict() == msg)
