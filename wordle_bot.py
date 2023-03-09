f65187d1122139dae9605753b9bf44f8eb273281
Sam Dupas <samgd1031@gmail.com>
2023-03-08 17:50:47 -0800

    moved commands out of main python file
    now organized into Cogs

--- wordle_bot.py
+++ wordle_bot.py
@@ -1,46 +1,30 @@
 import discord
 from discord.ext import commands
 import os
-from src.cogs import WordleUtilCog
 import src.util as util
 
 
 class Wordlebot(commands.Bot):
     def __init__(self, *, intents: discord.Intents, **options) -> None:
         super().__init__(intents=intents, **options, command_prefix='!')
+        self.remove_command("help")
 
     async def setup_hook(self) -> None:
-        await bot.add_cog(WordleUtilCog(bot))
-        await self.tree.sync()
+        await self.load_extensions()
+
+    async def load_extensions(self):
+        """load cogs"""
+        for file in os.listdir("./src/cogs"):
+            if file.endswith(".py"):
+                await self.load_extension(f"src.cogs.{file[:-3]}")
 
     async def on_ready(self):
         print("logged in")
 
 
-# set up the bot
-intents = discord.Intents.all()
-bot = Wordlebot(intents=intents)
-
-# slash commands for doing stuff
-@bot.tree.command(name="hello")
-async def hello(itx : discord.Interaction):
-    """Says hello"""
-    await itx.response.send_message(f"Hello {itx.user.name}", ephemeral=True)
-
-@bot.tree.command(name="get_puzzle")
-async def get_puzzle(itx: discord.Interaction, puzzle_num: int):
-    """Get the ranking for a given puzzle number"""
-
-    ucog = bot.get_cog("WordleUtilCog")
-    entries = ucog.get_puzzle_entries(puzzle_num, itx.guild_id)
-
-    # sort entries
-    entries = sorted(entries, key=lambda d: d['num_guesses'])
-
-    # build response
-    resp = util.print_daily_winners(entries, puzzle_num)
-
-    await itx.response.send_message(resp)
-
-# run the bot
-bot.run(token=os.environ["DISCORD_TOKEN"])
+if __name__ == '__main__':
+    # set up the bot
+    intents = discord.Intents.all()
+    bot = Wordlebot(intents=intents)
+    # run the bot
+    bot.run(token=os.environ["DISCORD_TOKEN"])
